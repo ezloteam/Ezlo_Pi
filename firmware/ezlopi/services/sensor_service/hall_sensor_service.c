aaf3265ada7a31aa5bc6656e8d2de5f565c69ca8
krishna kumar sah <krishna.kumar@ezlo.com>
2022-09-16 16:53:57 +0545

    refactored hall-sensor, switch, mpu6050, and dht11

--- firmware/ezlopi/services/sensor_service/hall_sensor_service.c
+++ firmware/ezlopi/services/sensor_service/hall_sensor_service.c
@@ -3,17 +3,17 @@
 
 #include "driver/adc.h"
 
+#include "debug.h"
 #include "items.h"
 #include "devices.h"
 #include "web_provisioning.h"
 #include "wss.h"
 
 static void hall_sensor_process(void *pv);
-static int hall_sensor_value = 0;
 static uint32_t device_index = 0;
+static int hall_sensor_value = 0;
 
-int hall_sensor_value_get(void)
-{
+int hall_sensor_value_get(void){
     return hall_sensor_value;
 }
 
@@ -26,15 +26,19 @@ void hall_sensor_service_init(uint32_t dev_idx)
 static void hall_sensor_process(void *pv)
 {
     adc1_config_width(ADC_WIDTH_BIT_12);
+    char value_buff[128];
 
     while (1) {
         hall_sensor_value = hall_sensor_read();
-        char *ret = items_update_with_device_index(NULL, 0, NULL, web_provisioning_get_message_count(), device_index);
         printf("Value: %d\r\n", hall_sensor_value);
 
+        memset(value_buff, 0, sizeof(value_buff));
+        snprintf(value_buff, sizeof(value_buff), "%s", (hall_sensor_value > 65 || hall_sensor_value < 15) ? "\"dw_is_closed\"" : "\"dw_is_opened\"");
+        char * ret = items_update_from_sensor(device_index, value_buff);
+        
         if (ret)
         {
-            // TRACE_W(">> DHT-service TX(ret): %s", ret);
+            TRACE_B(">> WS Tx - 'hub.item.updated' [%d]\r\n%s", strlen(ret), ret);
             wss_client_send(ret, strlen(ret));
             vPortFree(ret);
             ret = NULL;
