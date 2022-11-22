1acb600abdd14f630e4f429d681356764822099c
lomasSE89 <lomas.subedi@ezlo.com>
2022-11-22 17:50:39 +0545

    hall sensor serviece src.

--- firmware/ezlopi/services/sensor_service/hall_sensor_service.c
+++ firmware/ezlopi/services/sensor_service/hall_sensor_service.c
@@ -3,28 +3,17 @@
 
 #include "driver/adc.h"
 
-<<<<<<< HEAD
-=======
 #include "debug.h"
->>>>>>> qt
 #include "items.h"
 #include "devices.h"
 #include "web_provisioning.h"
 #include "wss.h"
 
 static void hall_sensor_process(void *pv);
-<<<<<<< HEAD
-static int hall_sensor_value = 0;
-static uint32_t device_index = 0;
-
-int hall_sensor_value_get(void)
-{
-=======
 static uint32_t device_index = 0;
 static int hall_sensor_value = 0;
 
 int hall_sensor_value_get(void){
->>>>>>> qt
     return hall_sensor_value;
 }
 
@@ -37,17 +26,6 @@ void hall_sensor_service_init(uint32_t dev_idx)
 static void hall_sensor_process(void *pv)
 {
     adc1_config_width(ADC_WIDTH_BIT_12);
-<<<<<<< HEAD
-
-    while (1) {
-        hall_sensor_value = hall_sensor_read();
-        char *ret = items_update_with_device_index(NULL, 0, NULL, web_provisioning_get_message_count(), device_index);
-        printf("Value: %d\r\n", hall_sensor_value);
-
-        if (ret)
-        {
-            // TRACE_W(">> DHT-service TX(ret): %s", ret);
-=======
     char value_buff[128];
 
     while (1) {
@@ -61,7 +39,6 @@ static void hall_sensor_process(void *pv)
         if (ret)
         {
             TRACE_B(">> WS Tx - 'hub.item.updated' [%d]\r\n%s", strlen(ret), ret);
->>>>>>> qt
             wss_client_send(ret, strlen(ret));
             vPortFree(ret);
             ret = NULL;
