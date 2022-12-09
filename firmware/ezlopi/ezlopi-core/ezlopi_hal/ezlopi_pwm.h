e1a14d732a3c62328975445dc5be06846b4ccad8
Krishna Kumar Sah <krishna.kumar@ezlo.com>
2022-12-09 19:07:06 +0545

    pwm_relsn: pwm resolution

--- firmware/ezlopi/ezlopi-core/ezlopi_hal/ezlopi_pwm.h
+++ firmware/ezlopi/ezlopi-core/ezlopi_hal/ezlopi_pwm.h
@@ -5,22 +5,23 @@
 
 #include "driver/ledc.h"
 
-typedef struct s_ezlopi_channel_speed{
+typedef struct s_ezlopi_channel_speed
+{
     uint32_t channel;
     uint32_t speed_mode;
-}s_ezlopi_channel_speed_t;
+} s_ezlopi_channel_speed_t;
 
-typedef struct ezlopi_pwm{
+typedef struct ezlopi_pwm
+{
     uint8_t gpio_num;
     uint32_t channel;
     uint32_t speed_mode;
-    uint8_t pwm_resln;
+    uint8_t pwm_resln; // pwm resolution
     uint32_t freq_hz;
     uint32_t duty_cycle;
-}s_ezlopi_pwm_t;
+} s_ezlopi_pwm_t;
 
-
-s_ezlopi_channel_speed_t* ezlopi_pwm_init(uint8_t pwm_gpio_num, uint8_t pwm_resln, uint32_t freq_hz, uint32_t duty_cycle);
+s_ezlopi_channel_speed_t *ezlopi_pwm_init(uint8_t pwm_gpio_num, uint8_t pwm_resln, uint32_t freq_hz, uint32_t duty_cycle);
 void ezlopi_pwm_change_duty(uint32_t channel, uint32_t speed, uint32_t duty);
 uint32_t ezlopi_pwm_get_duty(uint32_t channel, uint32_t speed);
 
