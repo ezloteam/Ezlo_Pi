1b71a7ea0e4bae7ad37b10757c980b76b7c05e9b
lomasSE89 <lomas.subedi@ezlo.com>
2023-04-11 09:44:17 +0545

    added gw and subnet

--- firmware/ezlopi/ezlopi-core/ezlopi_wifi/ezlopi_wifi.c
+++ firmware/ezlopi/ezlopi-core/ezlopi_wifi/ezlopi_wifi.c
@@ -33,8 +33,8 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
-#define EXAMPLE_ESP_WIFI_SSID "nepaldigisys"
-#define EXAMPLE_ESP_WIFI_PASS "NDS_0ffice"
+// #define EXAMPLE_ESP_WIFI_SSID "nepaldigisys"
+// #define EXAMPLE_ESP_WIFI_PASS "NDS_0ffice"
 #define EXAMPLE_ESP_MAXIMUM_RETRY 5
 
 /* FreeRTOS event group to signal when we are connected*/
