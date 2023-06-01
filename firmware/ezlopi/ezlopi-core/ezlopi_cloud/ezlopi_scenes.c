ea240ee1c6446522f51ecf0382733d1d428da7fe
Krishna Kumar Sah <krishna.kumar@ezlo.com>
2023-06-01 19:01:21 +0545

    working on scene logic, scene structure is complete

---
+++ firmware/ezlopi/ezlopi-core/ezlopi_cloud/ezlopi_scenes.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+#include "trace.h"
+
+#include "ezlopi_scenes.h"
+
+static l_scenes_list_t *scene_list = NULL;
+
+void ezlopi_scene_create(void)
+{
+}
+
+void ezlopi_scene_delete(void)
+{
+}
+
+void ezlopi_scene_init(void)
+{
+}
--- firmware/ezlopi/ezlopi-core/ezlopi_cloud/ezlopi_scenes.h
+++ firmware/ezlopi/ezlopi-core/ezlopi_cloud/ezlopi_scenes.h
@@ -31,19 +31,22 @@ typedef struct l_fields
         char value_int;
     };
     struct l_fields *next;
-}
+} l_fields_t;
 
 typedef struct l_then_block
 {
 
-    char block_type[32];
     s_block_options_t block_options;
+    char block_type[32];
+    l_fields_t *fields;
     struct l_then_block *next;
 } l_then_block_t;
 
 typedef struct l_when_block
 {
     s_block_options_t block_options;
+    char block_type[32];
+    l_fields_t *fields;
     struct l_when_block *next;
 } l_when_block_t;
 
@@ -59,7 +62,7 @@ typedef struct l_house_modes
     struct l_house_modes *next;
 } l_house_modes_t;
 
-typedef struct l_scenes_params
+typedef struct l_scenes_list
 {
     char _id[32];
     bool enabled;
@@ -68,7 +71,12 @@ typedef struct l_scenes_params
     char name[32];
     char parent_id[32];
 
-    struct l_scenes_params *next;
-} l_scenes_params_t;
+    l_user_notification_t *user_notifications;
+    l_house_modes_t *house_modes;
+    l_then_block_t *then;
+    l_when_block_t *when;
+
+    struct l_scenes_list *next;
+} l_scenes_list_t;
 
 #endif //  __EZLOPI_SCENES_H__
