fc2c750d6cb49721202a87bc516463b6a58b02a6
krishna kumar sah <krriss.shah@gmail.com>
2023-07-10 12:40:57 +0545

    adding scenes task

--- firmware/ezlopi/services/ezlopi_scenes_service/ezlopi_scenes_service.c
+++ firmware/ezlopi/services/ezlopi_scenes_service/ezlopi_scenes_service.c
@@ -6,78 +6,24 @@
 #include "ezlopi_scenes.h"
 #include "ezlopi_scenes_service.h"
 
-static void __scenes_process(void *pv);
+static void __scenes_process(void *arg);
 
 void ezlopi_scenes_service_init(void)
 {
-    xTaskCreate(__scenes_process, "Scenes Process", 2048, NULL, 2, NULL);
-}
-
-static int __execute_then_block(l_then_block_t *then_block)
-{
-    int ret = 0;
-    while (then_block)
+    l_scenes_list_t *curr_scene = ezlopi_scenes_get_scenes_list();
+    while (curr_scene)
     {
-
-        then_block = then_block->next;
+        xTaskCreate(__scenes_process, curr_scene->name, 2 * 2048, curr_scene, 2, NULL);
+        curr_scene = curr_scene->next;
     }
-
-    return ret;
 }
 
-static int __check_when_block(l_when_block_t *when_block)
+static void __scenes_process(void *arg)
 {
-    int ret = 0;
-    while (when_block)
-    {
-#if 0
-        switch (when_block->block_options.method.arg_type)
-        {
-        case EZLOPI_SCENE_ARG_TYPE_DEVICE:
-        {
-            break;
-        }
-        case EZLOPI_SCENE_ARG_TYPE_HTTP_REQUEST:
-        {
-            break;
-        }
-        case EZLOPI_SCENE_ARG_TYPE_HOUSE_MODE:
-        {
-            break;
-        }
-        case EZLOPI_SCENE_ARG_TYPE_LUA_SCRIPT:
-        {
-            break;
-        }
-        default:
-        {
-            TRACE_E("Arg type not defined!");
-            break;
-        }
-        }
-#endif
-        if (ret)
-        {
-            break;
-        }
-
-        when_block = when_block->next;
-    }
-    return ret;
-}
-
-static void __scenes_process(void *pv)
-{
+    l_scenes_list_t *scene = (l_scenes_list_t *)arg;
+    TRACE_B("task - '%s': Running", scene->name);
     while (1)
     {
-        l_scenes_list_t *scenes_list = ezlopi_scenes_get_scenes_list();
-        while (scenes_list)
-        {
-            if (__check_when_block(scenes_list->when))
-            {
-                __execute_then_block(scenes_list->then);
-            }
-            scenes_list = scenes_list->next;
-        }
+        vTaskDelay(100);
     }
 }
