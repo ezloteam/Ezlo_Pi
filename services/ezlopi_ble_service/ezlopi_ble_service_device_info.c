848f65564d959f70f965a0f501a9e78cf9f2947c
lomasSE89 <lomas.subedi@ezlo.com>
2023-07-11 18:30:25 +0545

    removed redundant info.

--- services/ezlopi_ble_service/ezlopi_ble_service_device_info.c
+++ services/ezlopi_ble_service/ezlopi_ble_service_device_info.c
@@ -109,12 +109,12 @@ static char *device_info_jsonify(void)
         cJSON_AddStringToObject(root, "firmware_version", VERSION_STR);
         cJSON_AddNumberToObject(root, "firmware_build", BUILD);
         cJSON_AddStringToObject(root, "chip", CONFIG_IDF_TARGET);
-        cJSON_AddStringToObject(root, "flash_size", CONFIG_ESPTOOLPY_FLASHSIZE);
-        cJSON_AddStringToObject(root, "version_idf", esp_get_idf_version());
+        // cJSON_AddStringToObject(root, "flash_size", CONFIG_ESPTOOLPY_FLASHSIZE);
+        // cJSON_AddStringToObject(root, "version_idf", esp_get_idf_version());
         cJSON_AddNumberToObject(root, "uptime", uptime_sec);
         cJSON_AddNumberToObject(root, "build_date", BUILD_DATE);
-        cJSON_AddNumberToObject(root, "boot_count", ezlopi_system_info_get_boot_count());
-        cJSON_AddNumberToObject(root, "boot_reason", esp_reset_reason());
+        // cJSON_AddNumberToObject(root, "boot_count", ezlopi_system_info_get_boot_count());
+        // cJSON_AddNumberToObject(root, "boot_reason", esp_reset_reason());
         cJSON_AddBoolToObject(root, "provisioned_status", ezlopi_factory_info_v2_get_provisioning_status());
 
         uint8_t ble_mac[6];
@@ -130,10 +130,10 @@ static char *device_info_jsonify(void)
         unsigned long long serial_id = ezlopi_factory_info_v2_get_id();
         cJSON_AddNumberToObject(root, "serial", serial_id);
         __add_factory_info_to_root(root, "ezlopi_device_type", ezlopi_factory_info_v2_get_device_type());
-        __add_factory_info_to_root(root, "model", ezlopi_factory_info_v2_get_model());
-        __add_factory_info_to_root(root, "device_name", ezlopi_factory_info_v2_get_name());
-        __add_factory_info_to_root(root, "brand", ezlopi_factory_info_v2_get_brand());
-        __add_factory_info_to_root(root, "manufacturer", ezlopi_factory_info_v2_get_manufacturer());
+        // __add_factory_info_to_root(root, "model", ezlopi_factory_info_v2_get_model());
+        // __add_factory_info_to_root(root, "device_name", ezlopi_factory_info_v2_get_name());
+        // __add_factory_info_to_root(root, "brand", ezlopi_factory_info_v2_get_brand());
+        // __add_factory_info_to_root(root, "manufacturer", ezlopi_factory_info_v2_get_manufacturer());
 
         char *ssid = ezlopi_factory_info_v2_get_ssid();
         if (ssid)
@@ -141,13 +141,13 @@ static char *device_info_jsonify(void)
             cJSON_AddStringToObject(root, "wifi_ssid", (isprint(ssid[0])) ? ssid : "");
         }
         esp_netif_ip_info_t *wifi_ip_info = ezlopi_wifi_get_ip_infos();
-        cJSON_AddStringToObject(root, "wifi-ip", ip4addr_ntoa((const ip4_addr_t *)&wifi_ip_info->ip));
-        cJSON_AddStringToObject(root, "wifi-gw", ip4addr_ntoa((const ip4_addr_t *)&wifi_ip_info->gw));
-        cJSON_AddStringToObject(root, "wifi-netmask", ip4addr_ntoa((const ip4_addr_t *)&wifi_ip_info->netmask));
+        // cJSON_AddStringToObject(root, "wifi-ip", ip4addr_ntoa((const ip4_addr_t *)&wifi_ip_info->ip));
+        // cJSON_AddStringToObject(root, "wifi-gw", ip4addr_ntoa((const ip4_addr_t *)&wifi_ip_info->gw));
+        // cJSON_AddStringToObject(root, "wifi-netmask", ip4addr_ntoa((const ip4_addr_t *)&wifi_ip_info->netmask));
         cJSON_AddNumberToObject(root, "wifi-connection_status", ezlopi_wifi_got_ip());
-        cJSON_AddStringToObject(root, "wifi-error", ezlopi_wifi_get_last_disconnect_reason());
-        const char *internet_status_str = (EZLOPI_PING_STATUS_LIVE == ezlopi_ping_get_internet_status()) ? "Internet available" : "Internet not available";
-        cJSON_AddStringToObject(root, "internet_status", internet_status_str);
+        // cJSON_AddStringToObject(root, "wifi-error", ezlopi_wifi_get_last_disconnect_reason());
+        uint8_t flag_internet_status = (EZLOPI_PING_STATUS_LIVE == ezlopi_ping_get_internet_status()) ? 1 : 0;
+        cJSON_AddNumberToObject(root, "internet_status", flag_internet_status);
 
         device_info = cJSON_Print(root);
         if (device_info)
