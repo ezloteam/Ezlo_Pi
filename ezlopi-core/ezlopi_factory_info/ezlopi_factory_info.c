6510b89a3b4202fbf5a7dc4635281621e69744b9
lomasSE89 <lomas.subedi@ezlo.com>
2023-07-26 16:57:02 +0545

    log level change.

--- ezlopi-core/ezlopi_factory_info/ezlopi_factory_info.c
+++ ezlopi-core/ezlopi_factory_info/ezlopi_factory_info.c
@@ -275,7 +275,7 @@ void ezlopi_factory_info_v2_get_ezlopi_mac(uint8_t *mac)
             }
             else
             {
-                TRACE_E("MAC: %x:%x:%x:%x:%x:%x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+                TRACE_I("MAC: %x:%x:%x:%x:%x:%x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
             }
         }
     }
