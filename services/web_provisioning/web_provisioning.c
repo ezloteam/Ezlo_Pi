ea3e6ea9068e0e84f2ddeb29ca804968fec5d8dc
lomasSE89 <lomas.subedi@ezlo.com>
2023-08-21 12:35:18 +0545

    fix memory crash issue.

--- services/web_provisioning/web_provisioning.c
+++ services/web_provisioning/web_provisioning.c
@@ -164,7 +164,7 @@ int web_provisioning_send_to_nma_websocket(cJSON *cjson_data, e_trace_type_t pri
 
 void web_provisioning_init(void)
 {
-    // xTaskCreate(web_provisioning_config_check, "web-provisioning config check", 4 * 2048, NULL, 5, NULL);
+    xTaskCreate(web_provisioning_config_check, "web-provisioning config check", 4 * 2048, NULL, 5, NULL);
     xTaskCreate(web_provisioning_fetch_wss_endpoint, "web-provisioning fetch wss endpoint", 3 * 2048, NULL, 5, &ezlopi_update_config_notifier);
 }
 
@@ -209,6 +209,7 @@ static void web_provisioning_config_check(void *pv)
         }
         if ((NULL != ca_certificate) && (NULL != provision_token) && (NULL != provisioning_server))
         {
+            TRACE_E("Here ");
 
             char http_request_location[200];
             snprintf(http_request_location, sizeof(http_request_location), "api/v1/controller/sync?version=%d", config_version); // add config_version instead of 1
@@ -242,6 +243,13 @@ static void web_provisioning_config_check(void *pv)
                 default:
                     break;
                 }
+                free(response->response);
+                free(response);
+            }
+            else
+            {
+                free(response);
+                TRACE_E("Here");
             }
             if (flag_break_loop)
             {
@@ -258,8 +266,6 @@ static void web_provisioning_config_check(void *pv)
         vTaskDelay(50000 / portTICK_RATE_MS);
     }
 
-    free(response->response);
-    free(response);
     free(provision_token);
     free(provisioning_server);
     vTaskDelete(NULL);
