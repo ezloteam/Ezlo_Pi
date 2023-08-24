dad83574835a28dd0e75d0e74bdc329e025c5e4c
krishna kumar sah <krriss.shah@gmail.com>
2023-08-24 17:39:18 +0545

    adding ds18b20 sensor: incomplete

--- firmware/ezlopi/devices/030_sens_ds18b20_sensor/030_sens_ds18b20_sensor.c
+++ firmware/ezlopi/devices/030_sens_ds18b20_sensor/030_sens_ds18b20_sensor.c
@@ -18,21 +18,104 @@
 #include "030_sens_ds18b20_sensor.h"
 #include "ds18b20_onewire.h"
 
+static int __prepare(void *arg);
+static int __init(l_ezlopi_item_t *item);
+static int __notify(l_ezlopi_item_t *item);
+static int __get_cjson_value(l_ezlopi_item_t *item, void *arg);
 
+int ds18b20_sensor(e_ezlopi_actions_t action, l_ezlopi_item_t *item, void *arg, void *user_arg)
+{
+    int ret = 0;
+    switch (action)
+    {
+    case EZLOPI_ACTION_PREPARE:
+    {
+        ret = __prepare(arg);
+        break;
+    }
+    case EZLOPI_ACTION_INITIALIZE:
+    {
+        ret = __init(item);
+        break;
+    }
+    case EZLOPI_ACTION_GET_EZLOPI_VALUE:
+    {
+        __get_cjson_value(item, arg);
+        break;
+    }
+    case EZLOPI_ACTION_NOTIFY_1000_MS:
+    {
+        __notify(item);
+        break;
+    }
+    default:
+    {
+        break;
+    }
+    }
+    return ret;
+}
+
+static void __prepare_device_cloud_properties(l_ezlopi_device_t *device, cJSON *cj_device)
+{
+    char *device_name = NULL;
+    CJSON_GET_VALUE_STRING(cj_device, "dev_name", device_name);
+
+    ASSIGN_DEVICE_NAME_V2(device, device_name);
+    device->cloud_properties.category = category_temperature;
+    device->cloud_properties.subcategory = subcategory_not_defined;
+    device->cloud_properties.device_type = dev_type_sensor;
+    device->cloud_properties.device_id = ezlopi_cloud_generate_device_id();
+}
+
+static void __prepare_item_properties(l_ezlopi_item_t *item, cJSON *cj_device)
+{
+    CJSON_GET_VALUE_INT(cj_device, "dev_type", item->interface_type);
+    item->cloud_properties.show = true;
+    item->cloud_properties.has_getter = true;
+    item->cloud_properties.has_setter = false;
+    item->cloud_properties.item_name = ezlopi_item_name_temp;
+    item->cloud_properties.value_type = value_type_temperature;
+    item->cloud_properties.item_id = ezlopi_cloud_generate_item_id();
+
+    CJSON_GET_VALUE_INT(cjson_device, "gpio", item->interface.onewire_master.onewire_pin);
+}
+
+static int __prepare(void *arg)
+{
+    int ret = 0;
+    s_ezlopi_prep_arg_t *prep_arg = (s_ezlopi_prep_arg_t *)arg;
+
+    if (prep_arg && prep_arg->cjson_device)
+    {
+        l_ezlopi_device_t *device = ezlopi_device_add_device();
+        if (device)
+        {
+            __prepare_device_cloud_properties(device, prep_arg->cjson_device);
+            l_ezlopi_item_t *item_temperature = ezlopi_device_add_item_to_device(device, ds18b20_sensor);
+            if (item_temperature)
+            {
+                __prepare_item_properties(item_temperature, prep_arg->cjson_device);
+            }
+        }
+    }
+
+    return ret;
+}
+
+#if 1
 static int ds18b20_sensor_prepare_and_add(void *args);
 static s_ezlopi_device_properties_t *ds18b20_sensor_prepare(cJSON *cjson_device);
 static int ds18b20_sensor_init(s_ezlopi_device_properties_t *properties);
 static int get_ds18b20_sensor_value_to_cloud(s_ezlopi_device_properties_t *properties, void *args);
 
-    
-static esp_err_t ds18b20_write_data(uint8_t* data, uint32_t gpio_pin);
-static esp_err_t ds18b20_read_data(uint8_t* data, uint32_t gpio_pin);
+static esp_err_t ds18b20_write_data(uint8_t *data, uint32_t gpio_pin);
+static esp_err_t ds18b20_read_data(uint8_t *data, uint32_t gpio_pin);
 static bool ds18b20_reset_line(uint32_t gpio_pin);
 static esp_err_t ds18b20_write_to_scratchpad(uint8_t th_val, uint8_t tl_val, uint8_t resolution, uint8_t gpio_pin);
 static bool ds18b20_recognize_device(uint32_t gpio_pin);
-static esp_err_t ds18b20_get_temperature_data(double* temperature_data, uint32_t gpio_pin);
-static uint8_t ds18b20_calculate_crc(const uint8_t* data, uint8_t len);
-
+static esp_err_t ds18b20_get_temperature_data(double *temperature_data, uint32_t gpio_pin);
+static uint8_t ds18b20_calculate_crc(const uint8_t *data, uint8_t len);
 
 int ds18b20_sensor(e_ezlopi_actions_t action, s_ezlopi_device_properties_t *ezlo_device, void *arg, void *user_arg)
 {
@@ -40,34 +123,33 @@ int ds18b20_sensor(e_ezlopi_actions_t action, s_ezlopi_device_properties_t *ezlo
 
     switch (action)
     {
-        case EZLOPI_ACTION_PREPARE:
-        {
-            ret = ds18b20_sensor_prepare_and_add(arg);
-            break;
-        }
-        case EZLOPI_ACTION_INITIALIZE:
-        {
-            ret = ds18b20_sensor_init(ezlo_device);
-            break;
-        }
-        case EZLOPI_ACTION_GET_EZLOPI_VALUE:
-        {
-            ret = get_ds18b20_sensor_value_to_cloud(ezlo_device, arg);
-            break;
-        }
-        case EZLOPI_ACTION_NOTIFY_1000_MS:
-        {
-            ezlopi_device_value_updated_from_device(ezlo_device);
-            break;
-        }
-        default:
-        {
-            break;
-        }
+    case EZLOPI_ACTION_PREPARE:
+    {
+        ret = ds18b20_sensor_prepare_and_add(arg);
+        break;
+    }
+    case EZLOPI_ACTION_INITIALIZE:
+    {
+        ret = ds18b20_sensor_init(ezlo_device);
+        break;
+    }
+    case EZLOPI_ACTION_GET_EZLOPI_VALUE:
+    {
+        ret = get_ds18b20_sensor_value_to_cloud(ezlo_device, arg);
+        break;
+    }
+    case EZLOPI_ACTION_NOTIFY_1000_MS:
+    {
+        ezlopi_device_value_updated_from_device(ezlo_device);
+        break;
+    }
+    default:
+    {
+        break;
+    }
     }
     return ret;
 }
-
 
 static int ds18b20_sensor_prepare_and_add(void *args)
 {
@@ -92,7 +174,6 @@ static int ds18b20_sensor_prepare_and_add(void *args)
 
     return ret;
 }
-
 
 static s_ezlopi_device_properties_t *ds18b20_sensor_prepare(cJSON *cjson_device)
 {
@@ -130,7 +211,6 @@ static s_ezlopi_device_properties_t *ds18b20_sensor_prepare(cJSON *cjson_device)
         ds18b20_sensor_properties->interface.gpio.gpio_in.mode = GPIO_MODE_DISABLE;
         ds18b20_sensor_properties->interface.gpio.gpio_in.pull = GPIO_FLOATING;
         ds18b20_sensor_properties->interface.gpio.gpio_in.value = 0;
-        
     }
     return ds18b20_sensor_properties;
 }
@@ -138,9 +218,9 @@ static s_ezlopi_device_properties_t *ds18b20_sensor_prepare(cJSON *cjson_device)
 static int ds18b20_sensor_init(s_ezlopi_device_properties_t *properties)
 {
     int ret = 0;
-    if(ds18b20_reset_line(properties->interface.gpio.gpio_in.gpio_num))
+    if (ds18b20_reset_line(properties->interface.gpio.gpio_in.gpio_num))
     {
-        if(ds18b20_recognize_device(properties->interface.gpio.gpio_in.gpio_num))
+        if (ds18b20_recognize_device(properties->interface.gpio.gpio_in.gpio_num))
         {
             TRACE_B("Providing initial settings to DS18B20.");
             ds18b20_write_to_scratchpad(DS18B20_TH_HIGHER_THRESHOLD, DS18B20_TL_LOWER_THRESHOLD, 12, properties->interface.gpio.gpio_in.gpio_num);
@@ -154,9 +234,9 @@ static int get_ds18b20_sensor_value_to_cloud(s_ezlopi_device_properties_t *prope
 {
     int ret = 0;
     double temperature = 0;
-    cJSON* cjson_properties = (cJSON*)args;
-    
-    if(cjson_properties)
+    cJSON *cjson_properties = (cJSON *)args;
+
+    if (cjson_properties)
     {
         ds18b20_get_temperature_data(&temperature, properties->interface.gpio.gpio_in.gpio_num);
         TRACE_B("Temperature is: %f degree censius", temperature);
@@ -167,14 +247,14 @@ static int get_ds18b20_sensor_value_to_cloud(s_ezlopi_device_properties_t *prope
     return ret;
 }
 
-static esp_err_t ds18b20_write_data(uint8_t* data, uint32_t gpio_pin)
+static esp_err_t ds18b20_write_data(uint8_t *data, uint32_t gpio_pin)
 {
     esp_err_t error = ESP_OK;
     error = one_wire_write_byte_to_line(data, gpio_pin);
     return error;
 }
 
-static esp_err_t ds18b20_read_data(uint8_t* data, uint32_t gpio_pin)
+static esp_err_t ds18b20_read_data(uint8_t *data, uint32_t gpio_pin)
 {
     esp_err_t error = ESP_OK;
     error = one_wire_read_byte_from_line(data, gpio_pin);
@@ -192,30 +272,30 @@ static esp_err_t ds18b20_write_to_scratchpad(uint8_t th_val, uint8_t tl_val, uin
 
     switch (resolution)
     {
-        case 9:
-        {
-            ds18b20_resolution = DS18B20_TEMPERATURE_9_BIT_RESOLUTION;
-            break;
-        }
-        case 10:
-        {
-            ds18b20_resolution = DS18B20_TEMPERATURE_10_BIT_RESOLUTION;
-            break;
-        }
-        case 11:
-        {
-            ds18b20_resolution = DS18B20_TEMPERATURE_11_BIT_RESOLUTION;
-            break;
-        }
-        case 12:
-        default:
-        {
-            ds18b20_resolution = DS18B20_TEMPERATURE_12_BIT_RESOLUTION;
-            break;
-        }
+    case 9:
+    {
+        ds18b20_resolution = DS18B20_TEMPERATURE_9_BIT_RESOLUTION;
+        break;
+    }
+    case 10:
+    {
+        ds18b20_resolution = DS18B20_TEMPERATURE_10_BIT_RESOLUTION;
+        break;
+    }
+    case 11:
+    {
+        ds18b20_resolution = DS18B20_TEMPERATURE_11_BIT_RESOLUTION;
+        break;
+    }
+    case 12:
+    default:
+    {
+        ds18b20_resolution = DS18B20_TEMPERATURE_12_BIT_RESOLUTION;
+        break;
+    }
     }
 
-    if(ds18b20_reset_line(gpio_pin))
+    if (ds18b20_reset_line(gpio_pin))
     {
         ds18b20_write_data(&ds18b20_skip_rom, gpio_pin);
         // write to the scratch pad. (TH, TL, Configuration) = (42, -10, 12-bit resolution.)
@@ -224,7 +304,7 @@ static esp_err_t ds18b20_write_to_scratchpad(uint8_t th_val, uint8_t tl_val, uin
         ds18b20_write_data(&ds18b20_tl_vla, gpio_pin);
         ds18b20_write_data(&ds18b20_resolution, gpio_pin);
     }
-    else 
+    else
     {
         error = ESP_FAIL;
     }
@@ -247,13 +327,13 @@ static bool ds18b20_recognize_device(uint32_t gpio_pin)
     return (data_from_ds18b20 == DS18B20_FAMILY_CODE) ? true : false;
 }
 
-static esp_err_t ds18b20_get_temperature_data(double* temperature_data, uint32_t gpio_pin)
+static esp_err_t ds18b20_get_temperature_data(double *temperature_data, uint32_t gpio_pin)
 {
     esp_err_t error = ESP_OK;
     uint8_t ds18b20_skip_rom = DS18B20_ROM_COMMAND_SKIP_ROM;
     uint8_t ds18b20_convert_temperature = DS18B20_FUNCTION_COMMAND_CONVERT_TEMP;
     uint8_t ds18b20_read_scratchpad = DS18B20_FUNCTION_COMMAND_READ_SCRATCHPAD;
-    
+
     uint8_t ds18b20_temperature_lsb = 0;
     uint8_t ds18b20_temperature_msb = 0;
     uint8_t ds18b20_th_value = 0;
@@ -264,7 +344,7 @@ static esp_err_t ds18b20_get_temperature_data(double* temperature_data, uint32_t
     uint8_t ds18b20_reserved_reg_3 = 0;
     uint8_t ds18b20_crc = 0;
 
-    if(ds18b20_reset_line(gpio_pin))
+    if (ds18b20_reset_line(gpio_pin))
     {
         ds18b20_write_data(&ds18b20_skip_rom, gpio_pin);
         ds18b20_write_data(&ds18b20_convert_temperature, gpio_pin);
@@ -296,38 +376,35 @@ static esp_err_t ds18b20_get_temperature_data(double* temperature_data, uint32_t
         };
 
         uint8_t calculated_crc = ds18b20_calculate_crc(ds18b20_scratchpad_data_array, 8);
-        if(calculated_crc != ds18b20_crc)
+        if (calculated_crc != ds18b20_crc)
         {
             TRACE_B("CRC not verified!!");
             error = ESP_FAIL;
         }
-        else 
+        else
         {
-            double temp=0;
-            temp=(double)(ds18b20_temperature_lsb+(ds18b20_temperature_msb*256.0))/16.0;
+            double temp = 0;
+            temp = (double)(ds18b20_temperature_lsb + (ds18b20_temperature_msb * 256.0)) / 16.0;
             *temperature_data = temp;
         }
     }
-    else 
+    else
     {
         error = ESP_FAIL;
     }
     return error;
 }
 
-
-static uint8_t ds18b20_calculate_crc(const uint8_t* data, uint8_t len)
+static uint8_t ds18b20_calculate_crc(const uint8_t *data, uint8_t len)
 {
     uint8_t crc = 0;
     uint8_t length = len;
-    while(length--)
+    while (length--)
     {
-        crc = *data++ ^ crc;  
-		crc = pgm_read_byte(dscrc2x16_table + (crc & 0x0f)) ^ pgm_read_byte(dscrc2x16_table + 16 + ((crc >> 4) & 0x0f));
+        crc = *data++ ^ crc;
+        crc = pgm_read_byte(dscrc2x16_table + (crc & 0x0f)) ^ pgm_read_byte(dscrc2x16_table + 16 + ((crc >> 4) & 0x0f));
     }
     return crc;
 }
 
-
-
-
+#endif
