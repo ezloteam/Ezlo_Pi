a82e8cc4444d25941946fd504c0df8d97dc2b0f0
ezlo-rikenm <riken.maharjan@ezlo.com>
2023-09-07 17:28:08 +0545

    adding MQ6-LPG sensor

--- ezlopi-core/ezlopi_factory_info/ezlopi_factory_info.h
+++ ezlopi-core/ezlopi_factory_info/ezlopi_factory_info.h
@@ -19,7 +19,7 @@ extern "C"
 #define EZLOPI_DEVICE_TYPE_SOUND_SENSOR 3
 #define EZLOPI_DEVICE_TYPE_AMBIENT_TRACKER_PRO 4
 
-#define EZLOPI_DEVICE_TYPE EZLOPI_DEVICE_TYPE_GENERIC
+#define EZLOPI_DEVICE_TYPE EZLOPI_DEVICE_TYPE_TEST_DEVICE
 
 #include "esp_partition.h"
 
@@ -407,14 +407,12 @@ static const char *test_device_constant_config =
         \"cmd\": 3,\
         \"dev_detail\":\
         [\{\
-                \"dev_name\": \"Joystick\",\
+                \"dev_name\": \"MQ6-LPG\",\
                 \"dev_type\": 10,\
-                \"id_item\": 20,\
+                \"id_item\": 59,\
                 \"id_room\": \"\",\
-                \"gpio_x\": 1,\
-                \"gpio_y\": 2,\
-                \"gpio_sw\": 3,\
-                \"resln_bit\": 10\
+                \"gpio1\": 25,\
+                \"gpio2\": 33\
             }\
         ],\
     \"dev_total\": 1}";
