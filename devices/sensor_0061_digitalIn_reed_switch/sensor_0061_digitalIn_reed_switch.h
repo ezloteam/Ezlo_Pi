a30a863bf78b20f34d59c92829e7f1c01109cb99
ezlo-rikenm <riken.maharjan@ezlo.com>
2023-09-12 17:59:33 +0545

    test

---
+++ devices/sensor_0061_digitalIn_reed_switch/sensor_0061_digitalIn_reed_switch.h
@@ -0,0 +1,9 @@
+#ifndef _SENSOR_0061_DIGITALIN_REED_SWITCH_H_
+#define _SENSOR_0061_DIGITALIN_REED_SWITCH_H_
+
+#include "ezlopi_actions.h"
+#include "ezlopi_devices.h"
+
+//----------------------------------------------------------------------
+
+#endif //_SENSOR_0061_DIGITALIN_REED_SWITCH_H_
