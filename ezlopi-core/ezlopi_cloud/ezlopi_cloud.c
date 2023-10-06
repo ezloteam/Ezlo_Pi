2dfe621e68d478ac1a3201e0381c42b69fa843a8
lomasSE89 <lomas.subedi@ezlo.com>
2023-10-06 10:56:52 +0545

    added gateway ID.

--- ezlopi-core/ezlopi_cloud/ezlopi_cloud.c
+++ ezlopi-core/ezlopi_cloud/ezlopi_cloud.c
@@ -14,7 +14,7 @@ static uint16_t ezlopi_get_mac_crc(void)
     uint16_t crc = 0;
     uint8_t mac_base[6] = {0};
     esp_efuse_mac_get_default((uint8_t *)mac_base);
-    
+
     // Perform CRC calculation on each byte of the MAC address
     for (int i = 0; i < 6; i++)
     {
@@ -34,14 +34,13 @@ static uint16_t ezlopi_get_mac_crc(void)
 
 uint32_t ezlopi_cloud_generate_device_id(void)
 {
-    if(0 == device_id)
+    if (0 == device_id)
     {
-        device_id =  0x10000000 + ezlopi_get_mac_crc();
-
+        device_id = 0x10000000 + ezlopi_get_mac_crc();
     }
-    else 
+    else
     {
-      device_id++;  
+        device_id++;
     }
     // TRACE_D("device_id: %u\r\n", device_id);
     return device_id;
@@ -49,12 +48,11 @@ uint32_t ezlopi_cloud_generate_device_id(void)
 
 uint32_t ezlopi_cloud_generate_item_id(void)
 {
-    if(0 == item_id)
+    if (0 == item_id)
     {
-        item_id =  0x20000000 + ezlopi_get_mac_crc();
-
+        item_id = 0x20000000 + ezlopi_get_mac_crc();
     }
-    else 
+    else
     {
         item_id++;
     }
@@ -65,12 +63,11 @@ uint32_t ezlopi_cloud_generate_item_id(void)
 
 uint32_t ezlopi_cloud_generate_room_id(void)
 {
-    if(0 == room_id)
+    if (0 == room_id)
     {
-        room_id =  0x30000000 + ezlopi_get_mac_crc();
-
+        room_id = 0x30000000 + ezlopi_get_mac_crc();
     }
-    else 
+    else
     {
         room_id++;
     }
@@ -80,11 +77,11 @@ uint32_t ezlopi_cloud_generate_room_id(void)
 
 uint32_t ezlopi_cloud_generate_gateway_id(void)
 {
-    if(0 == gateway_id)
+    if (0 == gateway_id)
     {
         // gateway_id =  0x40000000 + (uint32_t)ezlopi_get_mac_crc();
-        gateway_id =  0x40000000 ;
-
+        // gateway_id =  0x40000000 ;
+        gateway_id = 0x457a5069;
     }
     // TRACE_D("gateway_id: %u\r\n", gateway_id);
     return gateway_id;
@@ -92,10 +89,9 @@ uint32_t ezlopi_cloud_generate_gateway_id(void)
 
 uint32_t ezlopi_cloud_generate_settings_id(void)
 {
-    if(0 == settings_id)
+    if (0 == settings_id)
     {
-        settings_id =  0x50000000 + ezlopi_get_mac_crc();
-
+        settings_id = 0x50000000 + ezlopi_get_mac_crc();
     }
     else
     {
