9cd697b64a07bd6093821b8428ed9ccf63f67938
NDJ-Ezlo <nabin.dangi@ezlo.com>
2023-10-31 18:19:40 +0545

    Previous and current value comparision of lux and minor change in initialization of LTR303 ALS.

--- devices/sensor_0008_I2C_LTR303ALS/sensor_0008_I2C_LTR303ALS.c
+++ devices/sensor_0008_I2C_LTR303ALS/sensor_0008_I2C_LTR303ALS.c
@@ -1,6 +1,7 @@
 
 #include <string.h>
 #include "sdkconfig.h"
+#include "math.h"
 
 #include "cJSON.h"
 #include "trace.h"
@@ -87,7 +88,7 @@ static int __notify(l_ezlopi_item_t *item)
         ltr303_data_t temp_data;
         if (ESP_OK == ltr303_get_val(&temp_data))
         {
-            if (temp_data.lux != als_ltr303_data->lux)
+            if (fabs(als_ltr303_data->lux - temp_data.lux) > 0.2)
             {
                 als_ltr303_data->lux = temp_data.lux;
                 ezlopi_device_value_updated_from_device_v3(item);
@@ -102,7 +103,13 @@ static int __init(l_ezlopi_item_t *item)
 {
     int ret = 0;
 
-    ltr303_setup(item->interface.i2c_master.sda, item->interface.i2c_master.scl, true);
+    ltr303_data_t *als_ltr303_data = (ltr303_data_t *)item->user_arg;
+
+    if ((item->interface.i2c_master.enable) && (NULL != als_ltr303_data))
+    {
+        ltr303_setup(item->interface.i2c_master.sda, item->interface.i2c_master.scl, true);
+        ltr303_get_val(als_ltr303_data);
+    }
 
     return ret;
 }
