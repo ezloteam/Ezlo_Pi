1481fdbcdfcde5f6411745cd6af5e1c7c50fa06c
krishna kumar sah <krriss.shah@gmail.com>
2023-12-04 18:56:25 +0545

    test version released

--- components/version/include/version.h
+++ components/version/include/version.h
@@ -5,16 +5,16 @@ extern "C"
 {
 #endif
 #define V_TYPE 2
-#define BUILD_DATE 1701358388
+#define BUILD_DATE 1701421297
 #define MAJOR 3
 #define MINOR 0
 #define BATCH 4
-#define BUILD 179
-#define VERSION_STR "3.0.4.179"
+#define BUILD 180
+#define VERSION_STR "3.0.4.180"
 #define DEVELOPER "krriss.shah@gmail.com"
-#define COMPILE_TIME "Thu 30 Nov 2023 09:17:59 PM +0545"
+#define COMPILE_TIME "Fri 01 Dec 2023 02:46:24 PM +0545"
 #define CURRENT_BRANCH "dev_v3_device_config"
-#define COMMIT_HASH "08a00aa4073e9a0fc7105c3b5c7ef45c1f96d36e"
+#define COMMIT_HASH "7f1b3bd420ca8b1af68d42af3b9983ef042b105b"
 #ifdef __cplusplus
 }
 #endif
