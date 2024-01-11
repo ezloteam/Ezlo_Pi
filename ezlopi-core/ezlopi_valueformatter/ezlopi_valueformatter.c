f0ef65d74799581c6f8a803f536fba15b63fe9c0
lomasSE89 <lomas.subedi@ezlo.com>
2024-01-11 12:58:26 +0545

    value formatter improved.

--- ezlopi-core/ezlopi_valueformatter/ezlopi_valueformatter.c
+++ ezlopi-core/ezlopi_valueformatter/ezlopi_valueformatter.c
@@ -10,48 +10,85 @@ char *ezlopi_valueformatter_bool(bool val)
 
 char *ezlopi_valueformatter_float(float val)
 {
-#warning "malloc not checked"
+
     char *formatted_val_str = (char *)malloc(15);
-    snprintf(formatted_val_str, 15, "%.2f", val);
-    return formatted_val_str;
+    if (formatted_val_str)
+    {
+        snprintf(formatted_val_str, 15, "%.2f", val);
+        return formatted_val_str;
+    }
+    else
+    {
+        return NULL;
+    }
 }
 
 char *ezlopi_valueformatter_double(double val)
 {
-#warning "malloc not checked"
     char *formatted_val_str = (char *)malloc(15);
-    snprintf(formatted_val_str, 15, "%.2lf", val);
-    return formatted_val_str;
+    if (formatted_val_str)
+    {
+        snprintf(formatted_val_str, 15, "%.2lf", val);
+        return formatted_val_str;
+    }
+    else
+    {
+        return NULL;
+    }
 }
 
 char *ezlopi_valueformatter_int(int val)
 {
-#warning "malloc not checked"
     char *formatted_val_str = (char *)malloc(15);
-    snprintf(formatted_val_str, 15, "%d", val);
-    return formatted_val_str;
+    if (formatted_val_str)
+    {
+        snprintf(formatted_val_str, 15, "%d", val);
+        return formatted_val_str;
+    }
+    else
+    {
+        return NULL;
+    }
 }
 
 char *ezlopi_valueformatter_int32(int32_t val)
 {
-#warning "malloc not checked"
     char *formatted_val_str = (char *)malloc(15);
-    snprintf(formatted_val_str, 15, "%d", val);
-    return formatted_val_str;
+    if (formatted_val_str)
+    {
+        snprintf(formatted_val_str, 15, "%d", val);
+        return formatted_val_str;
+    }
+    else
+    {
+        return NULL;
+    }
 }
 
 char *ezlopi_valueformatter_uint32(uint32_t val)
 {
-#warning "malloc not checked"
     char *formatted_val_str = (char *)malloc(15);
-    snprintf(formatted_val_str, 15, "%d", val);
-    return formatted_val_str;
+    if (formatted_val_str)
+    {
+        snprintf(formatted_val_str, 15, "%d", val);
+        return formatted_val_str;
+    }
+    else
+    {
+        return NULL;
+    }
 }
 
 char *ezlopi_valueformatter_rgb(uint8_t r, uint8_t g, uint8_t b)
 {
-#warning "malloc not checked"
     char *formatted_val_str = (char *)malloc(10);
-    snprintf(formatted_val_str, 10, "#%02x%02x%02x", r, g, b);
-    return formatted_val_str;
+    if (formatted_val_str)
+    {
+        snprintf(formatted_val_str, 10, "#%02x%02x%02x", r, g, b);
+        return formatted_val_str;
+    }
+    else
+    {
+        return NULL;
+    }
 }
