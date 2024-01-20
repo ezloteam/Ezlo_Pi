cd7bd32f73ffde58991fbf15e418ce1017d78c36
lomasSE89 <lomas.subedi@ezlo.com>
2024-01-20 23:17:25 +0545

    build for v3.2.4 Build 182

--- components/version/include/version.h
+++ components/version/include/version.h
@@ -5,16 +5,16 @@ extern "C"
 {
 #endif
 #define V_TYPE 2
-#define BUILD_DATE 1704990285
+#define BUILD_DATE 1705766186
 #define MAJOR 3
-#define MINOR 1
+#define MINOR 2
 #define BATCH 4
-#define BUILD 181
-#define VERSION_STR "3.1.4"
+#define BUILD 182
+#define VERSION_STR "3.2.4"
 #define DEVELOPER "lomas.subedi@ezlo.com"
-#define COMPILE_TIME "Thu 11 Jan 2024 10:09:36 PM +0545"
+#define COMPILE_TIME "Sat 20 Jan 2024 09:41:25 PM +0545"
 #define CURRENT_BRANCH "dev_v3"
-#define COMMIT_HASH "fd190843f60c1e5a93c738b3b0757e58870a01c2"
+#define COMMIT_HASH "c413462f3f954a260acc26652a34a4293dcfcb58"
 #ifdef __cplusplus
 }
 #endif
