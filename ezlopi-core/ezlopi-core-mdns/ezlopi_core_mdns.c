7a6728f681c05e79812e0a2f8f399a48a3f194ad
NDJ-Ezlo <nabin.dangi@ezlo.com>
2024-02-26 16:38:42 +0545

    Added service to mDNS

--- ezlopi-core/ezlopi-core-mdns/ezlopi_core_mdns.c
+++ ezlopi-core/ezlopi-core-mdns/ezlopi_core_mdns.c
@@ -11,18 +11,20 @@
 #include "driver/gpio.h"
 #include "netdb.h"
 
+#include "ezlopi_core_factory_info.h"
+
 #include "ezlopi_util_trace.h"
 
 #include "ezlopi_core_mdns.h"
 
-#define EXAMPLE_MDNS_INSTANCE "esp-instance"
-#define EXAMPLE_HOST_NAME "ezlopi-device"
+#define EXAMPLE_MDNS_INSTANCE "esp_instance"
+#define EXAMPLE_HOST_NAME "ezlopi_device"
 
-static char *generate_hostname(void);
+static char* generate_hostname(void);
 
 static void initialise_mdns(void)
 {
-    char *hostname = generate_hostname();
+    char* hostname = generate_hostname();
 
     // initialize mDNS
     ESP_ERROR_CHECK(mdns_init());
@@ -31,7 +33,55 @@ static void initialise_mdns(void)
     TRACE_D("mdns hostname set to: [%s]", hostname);
     // set default mDNS instance name
     ESP_ERROR_CHECK(mdns_instance_name_set(EXAMPLE_MDNS_INSTANCE));
-    ESP_ERROR_CHECK(mdns_service_add("ESP32-WebServer", "_http", "_tcp", 80, NULL, 0));
+
+    uint64_t id_val = ezlopi_factory_info_v3_get_id();
+    char* id_val_str = malloc(10 * sizeof(char));
+    memset(id_val_str, 0, (10 * sizeof(char)));
+    snprintf(id_val_str, 10, "%lld", id_val);
+
+    uint16_t firmware_version_val = ezlopi_factory_info_v3_get_version();
+    char* firmware_version_val_str = malloc(10 * sizeof(char));
+    memset(firmware_version_val_str, 0, 10 * sizeof(char));
+    snprintf(firmware_version_val_str, 10, "%d", firmware_version_val);
+
+    // Define mdns serive context
+    mdns_txt_item_t service_context_item[8] =
+    {
+        {
+            .key = "ID",
+            .value = id_val_str,
+        },
+        {
+            .key = "name",
+            .value = (NULL == ezlopi_factory_info_v3_get_name() ? "null" : ezlopi_factory_info_v3_get_name()),
+        },
+        {
+            .key = "manufacturer",
+            .value = (NULL == ezlopi_factory_info_v3_get_manufacturer() ? "null" : ezlopi_factory_info_v3_get_manufacturer()),
+        },
+        {
+            .key = "brand",
+            .value = (NULL == ezlopi_factory_info_v3_get_brand() ? "null" : ezlopi_factory_info_v3_get_brand())
+        },
+        {
+            .key = "Model",
+            .value = (NULL == ezlopi_factory_info_v3_get_model() ? "null" : ezlopi_factory_info_v3_get_model()),
+        },
+        {
+            .key = "ezlopi_device_type",
+            .value = (NULL == ezlopi_factory_info_v3_get_device_type() ? "null" : ezlopi_factory_info_v3_get_device_type()),
+        },
+        {
+            .key = "ezlopi_firmware_version",
+            .value = firmware_version_val_str,
+        },
+        {
+            .key = "ezlopi_firmware_build",
+            .value = "1.8.2",
+        },
+    };
+
+    ESP_ERROR_CHECK(mdns_service_add("EzloPi_Serial", "_http", "_tcp", 80, service_context_item, 8));
     free(hostname);
 }
 
@@ -45,13 +95,13 @@ int ezlopi_core_initialize_mdns(void)
 /** Generate host name based on sdkconfig, optionally adding a portion of MAC address to it.
  *  @return host name string allocated from the heap
  */
-static char *generate_hostname(void)
+static char* generate_hostname(void)
 {
 #ifndef CONFIG_MDNS_ADD_MAC_TO_HOSTNAME
     return strdup(EXAMPLE_HOST_NAME);
 #else
     uint8_t mac[6];
-    char *hostname;
+    char* hostname;
     esp_read_mac(mac, ESP_MAC_WIFI_STA);
     if (-1 == asprintf(&hostname, "%s-%02X%02X%02X", EXAMPLE_HOST_NAME, mac[3], mac[4], mac[5]))
     {
