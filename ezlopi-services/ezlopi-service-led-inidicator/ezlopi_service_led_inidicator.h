ea7cafcdd00fe487e33adfd937ea2fc0649003f0
NDJ-Ezlo <nabin.dangi@ezlo.com>
2024-04-03 12:44:48 +0545

    Initial implementation of status LED in esp32s3

---
+++ ezlopi-services/ezlopi-service-led-inidicator/ezlopi_service_led_inidicator.h
@@ -0,0 +1,11 @@
+
+
+#ifndef _EZLOPI_SERVICE_LED_INDICATOR_H_
+#define _EZLOPI_SERVICE_LED_INDICATOR_H_
+
+
+int ezlopi_service_led_indicator_init();
+
+
+#endif // _EZLOPI_SERVICE_LED_INDICATOR_H_
+
