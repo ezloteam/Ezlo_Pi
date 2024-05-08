00c31d639918c7bf4c55e5aed00621b47f639357
lomasSE89 <lomas.subedi@ezlo.com>
2024-05-08 19:04:27 +0545

    added file and line on cloud log.

--- ezlopi-util/ezlopi-util-trace/ezlopi_util_trace.c
+++ ezlopi-util/ezlopi-util-trace/ezlopi_util_trace.c
@@ -99,7 +99,7 @@ void trace_color_print(const char* txt_color, uint8_t severity, const char* file
         va_list args;
         va_start(args, format);
         char cloud_log_format[EZPI_CORE_LOG_BUFFER_SIZE];
-        snprintf(cloud_log_format, sizeof(cloud_log_format), "[%s[%d]: ", txt_color, file, line);
+        snprintf(cloud_log_format, sizeof(cloud_log_format), "[File: %s Line: %d]: ", file, line);
         vsnprintf(cloud_log_format + strlen(cloud_log_format), sizeof(cloud_log_format) - strlen(cloud_log_format), format, args);
         log_upcall_func(severity, cloud_log_format);
         va_end(args);
@@ -114,7 +114,7 @@ void trace_color_print(const char* txt_color, uint8_t severity, const char* file
         snprintf(serial_log_format + strlen(serial_log_format), sizeof(serial_log_format) - strlen(serial_log_format), "\x1B[0m\r\n");
         log_upcall_func(severity, serial_log_format);
         va_end(args);
-}
+    }
 }
 
 #endif // ENABLE_TRACE
