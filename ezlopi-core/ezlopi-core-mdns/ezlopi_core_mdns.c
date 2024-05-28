ded49c4d6ff77edf6a7ac28f40e62de10349597c
lomasSE89 <lomas.subedi@ezlo.com>
2024-05-28 17:19:15 +0545

    mDNS name change and pre-release 3.8.9

--- ezlopi-core/ezlopi-core-mdns/ezlopi_core_mdns.c
+++ ezlopi-core/ezlopi-core-mdns/ezlopi_core_mdns.c
@@ -25,8 +25,6 @@
 
 #include "EZLOPI_USER_CONFIG.h"
 
-
-const char* ezlopi_mdns_instance_name = "EzloPi, an Open Source IoT Platform";
 static l_ezlopi_mdns_context_t* ezlopi_mdns_service_cntx = NULL;
 
 static void __mdns_init(void* pv);
@@ -278,7 +276,7 @@ static void __mdns_init(void* pv)
                 {
                     TRACE_I("\t%s\t%s", mdns_context[i].key, mdns_context[i].value);
                 }
-                mdns_service_add(ezlopi_mdns_instance_name, "_ezlo", "_tcp", 17001, mdns_context, service_size);
+                mdns_service_add("EzloPi", "_ezlo", "_tcp", 17001, mdns_context, service_size);
                 break;
             }
             else
