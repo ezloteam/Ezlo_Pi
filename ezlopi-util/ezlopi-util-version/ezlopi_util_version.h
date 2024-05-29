d9f63ea61990f31f384af91223794971ee890961
lomasSE89 <lomas.subedi@ezlo.com>
2024-05-29 12:20:20 +0545

    new version number.

--- ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
+++ ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
@@ -9,10 +9,10 @@ extern "C" {
 
 #define BUILD_DATE 1716896123
 #define MAJOR 3
-#define MINOR 8
+#define MINOR 5
 #define BATCH 9
 #define BUILD 189
-#define VERSION_STR "3.8.9"
+#define VERSION_STR "3.5.9"
 #define DEVELOPER "lomas.subedi@ezlo.com"
 #define COMPILE_TIME "Tue 28 May 2024 05:20:23 PM +0545"
 #define CURRENT_BRANCH "development"
