9472eca1a4d741b2f3f127dec9b61c1d3182a812
ezlo-rikenm <riken.maharjan@ezlo.com>
2024-07-01 18:17:16 +0545

    code refactoring

--- ezlopi-core/ezlopi-core-scenes/ezlopi_core_scenes_then_methods_helper_func.h
+++ ezlopi-core/ezlopi-core-scenes/ezlopi_core_scenes_then_methods_helper_func.h
@@ -20,5 +20,7 @@ void parse_http_skipsecurity(s_ezlopi_core_http_mbedtls_t* tmp_http_data, l_fiel
 void parse_http_creds(s_ezlopi_core_http_mbedtls_t* tmp_http_data, l_fields_v2_t* curr_field);
 void free_http_mbedtls_struct(s_ezlopi_core_http_mbedtls_t* config);
 
+int ezlopi_core_scene_then_helper_setexpression_setvariable(char * expression_name, const char * code_str, const char * value_type, cJSON * cj_metadata, cJSON * cj_params, l_fields_v2_t * var_value);
+
 #endif  // CONFIG_EZPI_SERV_ENABLE_MESHBOTS
 #endif /*_EZLOPI_CORE_SCENES_THEN_METHODS_HELPER_FUNCTIONS_H_*/
