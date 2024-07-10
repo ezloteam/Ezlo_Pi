1bea2b4a818a2b4b7e8d5b02f83a7c16422712a7
lomasSE89 <lomas.subedi@ezlo.com>
2024-07-10 20:27:43 +0545

    version update.

--- ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
+++ ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
@@ -7,16 +7,16 @@ extern "C" {
 #endif
 
 
-#define BUILD_DATE 1720621961
+#define BUILD_DATE 1720622547
 #define MAJOR 4
 #define MINOR 1
 #define BATCH 3
 #define BUILD 191
 #define VERSION_STR "4.1.3"
 #define DEVELOPER "lomas.subedi@ezlo.com"
-#define COMPILE_TIME "Wed Jul 10 08:17:41 PM +0545 2024"
+#define COMPILE_TIME "Wed Jul 10 08:27:27 PM +0545 2024"
 #define CURRENT_BRANCH "development"
-#define COMMIT_HASH "d3fa7429ff2a4da006222fbad055397daf1bfdc5"
+#define COMMIT_HASH "c2fbb27a518900e391a060655358b9f06429b0c0"
 
 
 #ifdef __cplusplus
