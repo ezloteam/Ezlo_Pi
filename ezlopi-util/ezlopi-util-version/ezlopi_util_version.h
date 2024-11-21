c943a6db922d11bf3911e05787b80024357dce01
lomasSE89 <lomas.subedi@ezlo.com>
2024-11-21 15:30:28 +0545

    updated version.

--- ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
+++ ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
@@ -1,27 +1,24 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-
-#define BUILD_DATE 1730365637
+#define BUILD_DATE 1732182210
 #define MAJOR 4
 #define MINOR 2
-#define BATCH 8
-#define BUILD 198
-#define VERSION_STR "4.2.8"
+#define BATCH 9
+#define BUILD 199
+#define VERSION_STR "4.2.9"
 #define DEVELOPER "lomas.subedi@ezlo.com"
-#define COMPILE_TIME "Thu Oct 31 02:52:17 PM +0545 2024"
+#define COMPILE_TIME "Thu Nov 21 09:43:30 AM +0000 2024"
 #define CURRENT_BRANCH "development"
-#define COMMIT_HASH "27c74ea104654e19393bf69449e072cb544b982c"
-
+#define COMMIT_HASH "1ccf59f81998ed635acf8eb1a8af52504a73002d"
 
 #ifdef __cplusplus
 }
 #endif
-
 
 #endif // __VERSION_H__
