e27b6601a11a974d3d2eaf33ef5c1959bb9fba51
Nabin-Flash320 <nabindangi32@gmail.com>
2024-12-18 12:31:34 +0545

    Updated cloud status to the src template

--- ezlopi-cloud/ezlopi-cloud-status/ezlopi_cloud_status.c
+++ ezlopi-cloud/ezlopi-cloud-status/ezlopi_cloud_status.c
@@ -1,36 +1,74 @@
 
+/**
+ * @file    ezlopi_cloud_status.c
+ * @brief
+ * @author
+ * @version
+ * @date
+ */
+/* ===========================================================================
+** Copyright (C) 2022 Ezlo Innovation Inc
+**
+** Under EZLO AVAILABLE SOURCE LICENSE (EASL) AGREEMENT
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are met:
+**
+** 1. Redistributions of source code must retain the above copyright notice,
+**    this list of conditions and the following disclaimer.
+** 2. Redistributions in binary form must reproduce the above copyright
+**    notice, this list of conditions and the following disclaimer in the
+**    documentation and/or other materials provided with the distribution.
+** 3. Neither the name of the copyright holder nor the names of its
+**    contributors may be used to endorse or promote products derived from
+**    this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+** ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+** LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+** CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+** SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+** INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+** CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+** POSSIBILITY OF SUCH DAMAGE.
+** ===========================================================================
+*/
+
 #include "esp_system.h"
 
 #include "ezlopi_util_trace.h"
 
-#include "ezlopi_cloud_constants.h"
-#include "ezlopi_cloud_status.h"
 #include "ezlopi_core_processes.h"
 
+#include "ezlopi_cloud_constants.h"
+#include "ezlopi_cloud_status.h"
 
-void EZPI_CLOUD_status_get(cJSON* cj_request, cJSON* cj_response)
+void EZPI_CLOUD_status_get(cJSON *cj_request, cJSON *cj_response)
 {
     cJSON_AddItemReferenceToObject(__FUNCTION__, cj_response, ezlopi_id_str, cJSON_GetObjectItem(__FUNCTION__, cj_request, ezlopi_id_str));
     cJSON_AddItemReferenceToObject(__FUNCTION__, cj_response, ezlopi_method_str, cJSON_GetObjectItem(__FUNCTION__, cj_request, ezlopi_method_str));
-    cJSON* cjson_result = cJSON_AddObjectToObject(__FUNCTION__, cj_response, ezlopi_result_str);
+    cJSON *cjson_result = cJSON_AddObjectToObject(__FUNCTION__, cj_response, ezlopi_result_str);
     if (cjson_result)
     {
-        cJSON* processes_array = cJSON_AddArrayToObject(__FUNCTION__, cjson_result, "processes");
+        cJSON *processes_array = cJSON_AddArrayToObject(__FUNCTION__, cjson_result, "processes");
         if (processes_array)
         {
             ezlopi_core_get_processes_details(processes_array);
         }
-        cJSON* cj_ram = cJSON_AddObjectToObject(__FUNCTION__, cjson_result, "ram");
+        cJSON *cj_ram = cJSON_AddObjectToObject(__FUNCTION__, cjson_result, "ram");
         if (cj_ram)
         {
             double free_heap_size = (esp_get_free_heap_size() / 1024.0);
-            cJSON* cj_available = cJSON_AddObjectToObject(__FUNCTION__, cj_ram, "available");
+            cJSON *cj_available = cJSON_AddObjectToObject(__FUNCTION__, cj_ram, "available");
             if (cj_available)
             {
                 cJSON_AddStringToObject(__FUNCTION__, cj_available, "scale", "Kb");
                 cJSON_AddNumberToObject(__FUNCTION__, cj_available, "value", free_heap_size);
             }
-            cJSON* cj_total_available_ram = cJSON_AddObjectToObject(__FUNCTION__, cj_ram, "total");
+            cJSON *cj_total_available_ram = cJSON_AddObjectToObject(__FUNCTION__, cj_ram, "total");
             if (cj_total_available_ram)
             {
                 cJSON_AddStringToObject(__FUNCTION__, cj_total_available_ram, "scale", "Kb");
@@ -42,4 +80,6 @@ void EZPI_CLOUD_status_get(cJSON* cj_request, cJSON* cj_response)
     }
 }
 
-
+/*******************************************************************************
+ *                          End of File
+ *******************************************************************************/
