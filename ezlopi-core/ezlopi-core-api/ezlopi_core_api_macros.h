a44b1bc82a20198e387e7175ccc1e4c49afb7d4e
Nabin-Flash320 <nabindangi32@gmail.com>
2024-12-18 12:14:51 +0545

    Updated cloud setting command to the src template

--- ezlopi-core/ezlopi-core-api/ezlopi_core_api_macros.h
+++ ezlopi-core/ezlopi-core-api/ezlopi_core_api_macros.h
@@ -144,7 +144,7 @@ CLOUD_METHOD("registered", EZPI_registered, NULL)
 
 CLOUD_METHOD("hub.data.list", EZPI_hub_data_list, NULL)
 
-CLOUD_METHOD("hub.setting.value.set", setting_value_set, setting_value_set_broadcast_updater)
-CLOUD_METHOD("hub.settings.list", settings_list, NULL)
+CLOUD_METHOD("hub.setting.value.set", EZPI_setting_value_set, EZPI_setting_value_set_broadcast_updater)
+CLOUD_METHOD("hub.settings.list", EZPI_settings_list, NULL)
 
 #endif // (defined(CONFIG_EZPI_WEBSOCKET_CLIENT) || defined(EZPI_LOCAL_WEBSOCKET_SERVER))
