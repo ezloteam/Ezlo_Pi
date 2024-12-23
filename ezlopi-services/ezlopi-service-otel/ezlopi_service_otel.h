6d9a034126227b3c1b07b330e27f12b1a3a8e22a
Krishna Kumar Sah <work.krishnasah@gmail.com>
2024-12-23 13:29:46 +0545

    added enums for kind and trace structure

--- ezlopi-services/ezlopi-service-otel/ezlopi_service_otel.h
+++ ezlopi-services/ezlopi-service-otel/ezlopi_service_otel.h
@@ -5,6 +5,7 @@
 
 #ifdef CONFIG_EZPI_ENABLE_OPENTELEMETRY
 #include "ezlopi_core_log.h"
+#include "ezlopi_core_api_methods.h"
 
 typedef enum e_otel_type
 {
@@ -14,6 +15,27 @@ typedef enum e_otel_type
     E_OTEL_MATRICS,
     E_OTEL_MAX,
 } e_otel_type_t;
+
+typedef enum e_otel_kind
+{
+    E_OTEL_KIND_INTERNAL = 0, // e.g.: meshbot, etc.
+    E_OTEL_KIND_SERVER = 1,   // e.g.: websocket server, uart, ble, etc.
+    E_OTEL_KIND_CLIENT = 2,   // e.g.: web-provisioning, http requests, etc.
+    E_OTEL_KIND_PRODUCER = 3,
+    E_OTEL_KIND_CONSUMER = 4
+
+} e_otel_kind_t;
+
+typedef struct s_otel_trace
+{
+    char *method;
+    uint32_t start_time;
+    uint32_t end_time;
+    uint32_t free_heap;
+    uint32_t heap_watermark;
+    e_otel_kind_t kind;
+
+} s_otel_trace_t;
 
 void ezlopi_service_otel_init(void);
 int ezlopi_service_otel_add_trace_to_telemetry_queue(cJSON *cj_trace);
