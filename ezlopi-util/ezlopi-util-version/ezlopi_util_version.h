705a1a12a15c05171138a8d4be1e8596e19676ca
lomasSE89 <lomas.subedi@ezlo.com>
2025-01-27 18:04:39 +0545

    update release note, release.

--- ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
+++ ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
@@ -6,16 +6,16 @@ extern "C"
 {
 #endif
 
-#define BUILD_DATE 1737976268
+#define BUILD_DATE 1737979007
 #define MAJOR 4
 #define MINOR 4
 #define PATCH 13
 #define BUILD 203
 #define VERSION_STR "4.4.13"
 #define DEVELOPER "lomas.subedi@ezlo.com"
-#define COMPILE_TIME "2025-01-27T16:56:08+0545"
+#define COMPILE_TIME "2025-01-27T17:41:47+0545"
 #define CURRENT_BRANCH "development"
-#define COMMIT_HASH "8c781cd69bd00588446c906cdb3060820d631f8c"
+#define COMMIT_HASH "0d9fd814d030ad06759560958ceb96683e86a4a1"
 
 #ifdef __cplusplus
 }
