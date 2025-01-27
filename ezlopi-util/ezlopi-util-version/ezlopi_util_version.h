57c867168192eca6af4be097e7f3f674c21cc32c
lomasSE89 <lomas.subedi@ezlo.com>
2025-01-27 16:56:25 +0545

    version file update.

--- ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
+++ ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
@@ -1,84 +1,24 @@
-/* ===========================================================================
-** Copyright (C) 2025 Ezlo Innovation Inc
-**
-** Under EZLO AVAILABLE SOURCE LICENSE (EASL) AGREEMENT
-**
-** Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are met:
-**
-** 1. Redistributions of source code must retain the above copyright notice,
-**    this list of conditions and the following disclaimer.
-** 2. Redistributions in binary form must reproduce the above copyright
-**    notice, this list of conditions and the following disclaimer in the
-**    documentation and/or other materials provided with the distribution.
-** 3. Neither the name of the copyright holder nor the names of its
-**    contributors may be used to endorse or promote products derived from
-**    this software without specific prior written permission.
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-** AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-** IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-** ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-** LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-** CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-** SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-** INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-** CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-** ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-** POSSIBILITY OF SUCH DAMAGE.
-** ===========================================================================
-*/
-
-/**
- * @file    main.c
- * @brief   perform some function on data
- * @author  
- * @version 0.1
- * @date    1st January 2024
- */
-
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-/*******************************************************************************
- *                          Include Files
- *******************************************************************************/
-
-/*******************************************************************************
- *                          C++ Declaration Wrapper
- *******************************************************************************/
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-    /*******************************************************************************
-     *                          Type & Macro Declarations
-     *******************************************************************************/
-#define BUILD_DATE 1735906897
+
+#define BUILD_DATE 1737976268
 #define MAJOR 4
 #define MINOR 4
-#define PATCH 12
-#define BUILD 202
-#define VERSION_STR "4.4.12"
+#define PATCH 13
+#define BUILD 203
+#define VERSION_STR "4.4.13"
 #define DEVELOPER "lomas.subedi@ezlo.com"
-#define COMPILE_TIME "2025-01-03T18:06:37+0545"
+#define COMPILE_TIME "2025-01-27T16:56:08+0545"
 #define CURRENT_BRANCH "development"
-#define COMMIT_HASH "a8d37c757afb6c58aeb42be9a9a114508538e688"
-
-    /*******************************************************************************
-     *                          Extern Data Declarations
-     *******************************************************************************/
-
-    /*******************************************************************************
-     *                          Extern Function Prototypes
-     *******************************************************************************/
+#define COMMIT_HASH "8c781cd69bd00588446c906cdb3060820d631f8c"
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif // __VERSION_H__
-
-/*******************************************************************************
- *                          End of File
- *******************************************************************************/
