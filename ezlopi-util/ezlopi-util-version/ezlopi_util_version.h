df41d2a14ef71f4d4622975331636f5956a2615a
lomasSE89 <lomas.subedi@ezlo.com>
2025-01-31 16:34:15 +0545

    minor string init fixes.

--- ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
+++ ezlopi-util/ezlopi-util-version/ezlopi_util_version.h
@@ -6,16 +6,16 @@ extern "C"
 {
 #endif
 
-#define BUILD_DATE 1737979007
+#define BUILD_DATE 1738318192
 #define MAJOR 4
 #define MINOR 4
-#define PATCH 13
-#define BUILD 203
-#define VERSION_STR "4.4.13"
+#define PATCH 14
+#define BUILD 204
+#define VERSION_STR "4.4.14"
 #define DEVELOPER "lomas.subedi@ezlo.com"
-#define COMPILE_TIME "2025-01-27T17:41:47+0545"
+#define COMPILE_TIME "2025-01-31T15:54:52+0545"
 #define CURRENT_BRANCH "development"
-#define COMMIT_HASH "0d9fd814d030ad06759560958ceb96683e86a4a1"
+#define COMMIT_HASH "648f70f46039c8a22452bb761a4f9465dd067436"
 
 #ifdef __cplusplus
 }
